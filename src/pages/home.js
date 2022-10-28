cb62c3d7c844a24586e8b86cc544a69ff11311ed
Sofi <sofi-lay@hotmail.com>
2022-10-28 15:41:44 -0300

    practice on framer-motion

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,11 @@
 import * as React from "react";
+import {motion}  from 'framer-motion'
 import { Link } from "react-router-dom";
 import ProgressiveImage from "react-progressive-image";
+
+const transition = {
+  duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96]
+}
 
 const Home = ({ imageDetails, image }) => (
   <>
@@ -20,18 +25,18 @@ const Home = ({ imageDetails, image }) => (
                   <ProgressiveImage
                     src={require("../images/yasmeen.webp")}
                     placeholder={require("../images/compressed-image.jpg")}>
-                    {(src) => <img src={src} alt='Yasmeen Tariq' />}
+                    {(src) => <motion.img whileHover={{ scale: 1.1 }} transition={transition} src={src} alt='Yasmeen Tariq' />}
                   </ProgressiveImage>
                 </Link>
               </div>
             </div>
-            <div className='information'>
+            <motion.div exit={{opacity: 0}} transition={transition} className='information'>
               <div className='title'>Yasmeen Tariq</div>
               <div className='location'>
                 <span>28.538336</span>
                 <span>-81.379234</span>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
